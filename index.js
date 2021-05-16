f5b717ea085438a986364c4f468236e1440851c1
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-05-16 08:27:25 -0300

    Added POST save-questions  route

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get("/",(req, res) => {
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+app.get("/",(req, res) => {
     res.render('index');
 });
 
@@ -13,7 +16,11 @@ app.get("/questions", (req, res) =>{
     res.render('questions');
 })
 
-app.get("/save-questions");
+app.post("/save-questions", (req, res) => {
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send("Formulario recebido! Título " + title + "; Descricao "+ description + ";");
+});
 
 app.listen(8080,() =>{
     console.log("App rodando!");
