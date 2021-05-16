450d5d8b65837f389f32abdb3989a1bc160a000d
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-05-16 10:59:02 -0300

    Added Answer model

---
+++ database/Answer.js
@@ -0,0 +1,18 @@
+const { TEXT, NUMBER } = require("sequelize");
+const Sequelize = require("sequelize");
+const connection = require("./database");
+
+const Answer = connection.define("answers", {
+    body: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    questionId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
+
+Answer.sync({force: false});
+
+module.exports = Answer;
